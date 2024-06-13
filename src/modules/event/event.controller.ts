053eb4dc9b889549dfa9423fc0ad7719a0334108
Fausto Zambrano <fausto@faqx.com>
2024-06-13 12:45:12 -0500

    adding missing swagger and Auth Decorators

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventType } from './interfaces/event.interface';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guard';
 
+@ApiTags('Preference')
 @Controller('events')
 export class EventController {
     constructor(
@@ -9,12 +12,18 @@ export class EventController {
     ) {}
 
     // Get All Existing Events
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOperation({ summary: 'Get All Events' })
     @Get()
     async getAllEvents(): Promise<EventType[]> {
         return this.eventService.findAllEvents();
     }
 
     // Create New Event
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOperation({ summary: 'Create New Event' })
     @Post('new')
     async createEvent(@Body() event: EventType): Promise<EventType> {
 
@@ -22,6 +31,9 @@ export class EventController {
     }
 
     // Get Event By ID
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOperation({ summary: 'Get Event By ID' })
     @Get(':id')
     async getEvent(
         @Param('id')
@@ -31,12 +43,18 @@ export class EventController {
     }
 
     // Update Event
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOperation({ summary: 'Update Event' })
     @Put(':id')
     async updateEvent(@Param('id') id: string, @Body() event: EventType): Promise<EventType> {
         return this.eventService.updateEventById(id, event);
     }
 
     // Delete Event
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOperation({ summary: 'Delete Event' })
     @Delete(':id')
     async deleteEvent(@Param('id') id: string): Promise<EventType> {
         return this.eventService.deleteEventById(id);
