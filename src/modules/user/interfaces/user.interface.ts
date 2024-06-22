128dda637725fd63ad9cfd617b0ef928cd7ea314
Rafael Carmona <rafael.carmona@tekbees.com>
2024-06-21 21:00:11 -0500

    changed

--- src/modules/user/interfaces/user.interface.ts
+++ src/modules/user/interfaces/user.interface.ts
@@ -1,13 +1,11 @@
 import { Document } from 'mongoose';
 import { DocumentTypeEnum } from '../enums/document-type.enum';
-
 export interface User extends Document {
   name: string;
   lastname: string;
   email: string;
   password: string;
   address: string;
-  bithDate: Date;
   user_status: string;
   documentType: DocumentTypeEnum;
   created_at: Date;
