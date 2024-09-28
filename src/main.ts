ddfb0b22da6bdb4feed85edccd5a6e90a87fac8a
Fausto Zambrano <fausto@faqx.com>
2024-09-28 08:59:12 -0500

    adding new cors config

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,12 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   app.use(cookieParser());
   app.use(helmet());
 
