2d73a91d3bed58b200bbe0447f8b9032acb4a83b
Fausto Zambrano <d.zambrano.vasconez@gmail.com>
2024-11-08 14:09:50 -0500

    adding new rules order

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -36,7 +36,7 @@ export class OrderService {
             const planString = order.item.split('-');
             const dayNumber = parseInt(planString[1]);
             current.plan = [planString[0]];
-            current.quota = parseInt(planString[1]);
+            current.quota = planString[0] === "pro" ? 3 : 8;
             const today = new Date()
             const dealine = new Date(today);
             dealine.setDate(today.getDate() + dayNumber);
